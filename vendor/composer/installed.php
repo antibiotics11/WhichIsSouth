7b65fc4a9e12b8ca4535bbc8978086a83cb7332f
ANTIBIOTICS <abx@abx.pe.kr>
2023-12-01 00:55:54 +0900

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '055ffa34e7a41823190c7f69f30733119150d548',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '055ffa34e7a41823190c7f69f30733119150d548',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
